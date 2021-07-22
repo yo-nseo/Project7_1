627ff1f9d03c56793b32ea9826576ea9ea20e6f7
YoonSeo4618 <w2035@e-mirim.hs.kr>
2021-07-22 10:47:01 +0900

    menu2

---
+++ app/src/main/java/kr/hs/emirim/ohyoonseo/project7_1/MainActivity2.java
@@ -0,0 +1,14 @@
+package kr.hs.emirim.ohyoonseo.project7_1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+    }
+}
